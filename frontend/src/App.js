bcb475983ae68ff1ad033caa36b73d9cc4f03fcd
9811611687 <40513559+9811611687@users.noreply.github.com>
2023-11-26 04:06:59 +0530

    pages+components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { UserContextProvider } from "./UserContext.js";
+import OneBlog from "./pages/OneBlog.js";
+import Blogs from "./pages/Blogs.js";
+import Home from "./pages/Home.js";
+import { Route, Routes } from "react-router-dom";
+import EditBlog from "./pages/EditBlog.js";
+import AddNewBlogs from "./components/AddNewBlogs.js";
+import NewBlog from "./components/NewBlog.js";
+import AddBlogs from "./pages/AddBlogs.js";
+import Login from "./pages/Login.js";
+import Register from "./pages/Register.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/blogs" element={<Blogs/>}/> */}
+        <Route path="/blogs" element={<OneBlog/>}/>
+        <Route path ="/edit" element={<EditBlog/>}/>
+        <Route path='/new' element={<AddBlogs/>}/>
+        <Route path ="/register" element={<Register/>}/>
+        <Route path ="/login" element={<Login/>}/>
+        
+        
+      </Routes>
+    </UserContextProvider>
   );
 }
 
